c78847e79b91ea32cd4d4f33bd1c56b523cd18eb
Tanishaa08 <vtanisha068@gmail.com>
2023-08-13 11:47:25 +0530

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+import { createAuth0Client } from '@auth0/auth0-spa-js';
 document.querySelectorAll('.image-container img').forEach(image => {
     image.onclick = () => {
         document.querySelector('.pop-image').style.display = 'block';
